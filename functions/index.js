b7a617a0fb83876f3465edf63234768aebff28d5
TakashiOshikawa <abr3tks5200@gmail.com>
2017-07-04 09:43:47 +0900

    Add addUser method.

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -6,3 +9,39 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
+
+// original idでのユーザ追加
+exports.addUser = functions.https.onRequest((req, res) => {
+  // 追加するユーザの定義
+  const user = {
+    "authentication_id_1" : {
+      "name": "test user",
+      "introduction": "自己紹介です",
+      "profile_image_path": "someimagepath"
+    }
+  };
+
+  // .pushを使うと独自に付与されるidの下にauthentication_id_1が付くのでユーザ追加には不向き
+  // メッセージなどには良いと思う
+  admin.database().ref('/users').push(user).then(snapshot => {
+    res.send(user);
+  });
+});
+
+// firebase authで取得したuidでのユーザ追加/更新
+// authentication_id_1を外部から渡された時に本人確認をどう行うか
+exports.setUser = functions.https.onRequest((req, res) => {
+  const user = {
+    "authentication_id_1" : {
+      "name": "test user",
+      "introduction": "自己紹介です",
+      "profile_image_path": "someimagepath"
+    }
+  };
+
+  admin.database().ref('/users').set(user).then(() => {
+    res.send(user);
+  });
+});
+
+
